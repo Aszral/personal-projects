87f493f4e3374a3e0dc2e4dfaa177e07fc016460
Luis Gonzalez <lg222zm@student.lnu.se>
2023-10-11 15:51:06 +0200

    Session management implemented

--- projects/code-image-gen/app.py
+++ projects/code-image-gen/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session
+from flask import Flask, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
 app.secret_key = "03c81118b363e3feaf4b7cc0c7cca9a298c45cf0c4fb5bf975604e67f4bf5856"
@@ -15,3 +15,16 @@ def code():
         "code": session["code"],
     }
     return render_template("code_input.html", **context)
+
+
+@app.route("/save_code", methods=["POST"])
+def save_code():
+    session["code"] = request.form.get("code")
+    return redirect(url_for("code"))
+
+
+@app.route("/reset_session", methods=["POST"])
+def reset_session():
+    session.clear()
+    session["code"] = PLACEHOLDER_CODE
+    return redirect(url_for("code"))
